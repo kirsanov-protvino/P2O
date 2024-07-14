68ea8a1ffbaf5c0585593ef4c3128416382dd866
kirsanov <Mikhail.Kirsanov@cern.ch>
2024-07-14 13:05:44 +0300

    Update Note 24-01

--- papers/P2O-NOTE-24-01/del.C
+++ papers/P2O-NOTE-24-01/del.C
@@ -2,7 +2,20 @@
   c1 = new TCanvas("del"," ",200,10,900,600);
 
   TGraph* graph_del = new TGraph("del_05.dat");
-  graph_del->Draw();
-  TGraph* graph_del1 = new TGraph("del_15.dat");
-  graph_del1->Draw("SAME");
+  graph_del->SetTitle("");
+  graph_del->SetLineWidth(3);
+  graph_del->SetLineColor(kBlue);
+  graph_del->GetXaxis()->SetTitle("Hypothetical #delta");
+  graph_del->GetYaxis()->SetTitle("#sqrt{#chi^{2}}");
+  graph_del->Draw("AC");
+
+  TGraph* graph_del_15 = new TGraph("del_15.dat");
+  graph_del_15->SetLineWidth(3);
+  graph_del_15->SetLineColor(kRed);
+  graph_del_15->Draw("C SAME");
+
+  TGraph* graph_del_1 = new TGraph("del_1.dat");
+  graph_del_1->SetLineWidth(3);
+  graph_del_1->SetLineColor(kGreen);
+  graph_del_1->Draw("C SAME");
 }
--- papers/P2O-NOTE-24-01/del_delres_eres.C
+++ papers/P2O-NOTE-24-01/del_delres_eres.C
@@ -3,7 +3,7 @@
 
   TGraph* graph_del = new TGraph("del_delres_eres.dat");
   graph_del->SetTitle("");
-  graph_del->SetLineColor(2);
+  graph_del->SetLineColor(kBlue);
   graph_del->SetLineWidth(3);
   graph_del->GetXaxis()->SetTitle("Energy resolution #DeltaE/E");
   graph_del->GetYaxis()->SetTitle("#Delta#delta, degrees");
@@ -11,13 +11,18 @@
   graph_del->Draw("AC");
 
   TGraph* graph_del_15 = new TGraph("del_delres_eres_15.dat");
-  graph_del_15->SetLineColor(3);
+  graph_del_15->SetLineColor(kRed);
   graph_del_15->SetLineWidth(3);
   graph_del_15->Draw("C SAME");
 
   TGraph* graph_del_1 = new TGraph("del_delres_eres_1.dat");
-  graph_del_1->SetLineColor(4);
+  graph_del_1->SetLineColor(kGreen);
   graph_del_1->SetLineWidth(3);
   graph_del_1->Draw("C SAME");
+
+  TGraph* graph_del_0 = new TGraph("del_delres_eres_0.dat");
+  graph_del_0->SetLineColor(kBlack);
+  graph_del_0->SetLineWidth(3);
+  graph_del_0->Draw("C SAME");
 
 }
