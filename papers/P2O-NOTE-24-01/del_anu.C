987a49b4bc2decb705fe110f7c73a6eee3b0a63b
kirsanov <Mikhail.Kirsanov@cern.ch>
2024-10-18 13:10:07 +0200

    Update Note 01

--- papers/P2O-NOTE-24-01/del_anu.C
+++ papers/P2O-NOTE-24-01/del_anu.C
@@ -1,7 +1,7 @@
 {
-  auto c1 = new TCanvas("del"," ",200,10,900,600);
+  auto c1 = new TCanvas("del_anu"," ",200,10,900,600);
 
-  TGraph* graph_del = new TGraph("del_05.dat");
+  TGraph* graph_del = new TGraph("del_05_anu.dat");
   graph_del->SetTitle("");
   graph_del->SetLineWidth(3);
   graph_del->SetLineColor(kBlue);
@@ -9,12 +9,12 @@
   graph_del->GetYaxis()->SetTitle("#sqrt{#chi^{2}}");
   graph_del->Draw("AC");
 
-  TGraph* graph_del_15 = new TGraph("del_15.dat");
+  TGraph* graph_del_15 = new TGraph("del_15_anu.dat");
   graph_del_15->SetLineWidth(3);
   graph_del_15->SetLineColor(kRed);
   graph_del_15->Draw("C SAME");
 
-  TGraph* graph_del_1 = new TGraph("del_1.dat");
+  TGraph* graph_del_1 = new TGraph("del_1_anu.dat");
   graph_del_1->SetLineWidth(3);
   graph_del_1->SetLineColor(kGreen);
   graph_del_1->Draw("C SAME");
