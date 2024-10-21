717cf20145370481a5320728cac5400aeade971f
kirsanov <Mikhail.Kirsanov@cern.ch>
2024-10-21 16:47:59 +0200

    Add files in Note 01

---
+++ papers/P2O-NOTE-24-01/del_anu_4.C
@@ -0,0 +1,27 @@
+{
+  auto c1 = new TCanvas("del_anu_4"," ",200,10,900,600);
+  c1->SetGrid();
+
+  TGraph* graph_del = new TGraph("del_05_anu.dat");
+  graph_del->SetTitle("");
+  graph_del->SetLineWidth(3);
+  graph_del->SetLineColor(kBlue);
+  graph_del->GetXaxis()->SetTitle("Hypothetical #delta");
+  graph_del->GetYaxis()->SetTitle("#sqrt{#chi^{2}}");
+  graph_del->Draw("AC");
+
+  TGraph* graph_del_15 = new TGraph("del_15_anu.dat");
+  graph_del_15->SetLineWidth(3);
+  graph_del_15->SetLineColor(kRed);
+  graph_del_15->Draw("C SAME");
+
+  TGraph* graph_del_1 = new TGraph("del_1_anu.dat");
+  graph_del_1->SetLineWidth(3);
+  graph_del_1->SetLineColor(kGreen);
+  graph_del_1->Draw("C SAME");
+
+  TGraph* graph_del_0 = new TGraph("del_0_anu.dat");
+  graph_del_0->SetLineWidth(3);
+  //graph_del_0->SetLineColor(kGreen);
+  graph_del_0->Draw("C SAME");
+}
