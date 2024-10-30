955999dd7d691e648ff7b6f9a1a41b7fd776ea1d
kirsanov <Mikhail.Kirsanov@cern.ch>
2024-10-30 15:38:30 +0100

    Update Note 01

---
+++ papers/P2O-NOTE-24-01/del_delres_eres_anu.C
@@ -0,0 +1,30 @@
+{
+  auto c1 = new TCanvas("del_delres_eres_anu"," ",200,10,900,600);
+  c1->SetGrid();
+
+  TGraph* graph_del = new TGraph("del_delres_eres_anu.dat");
+  graph_del->SetTitle("");
+  graph_del->SetLineColor(kBlue);
+  graph_del->SetLineWidth(3);
+  graph_del->GetXaxis()->SetTitle("Energy resolution #DeltaE/E");
+  graph_del->GetYaxis()->SetTitle("#Delta#delta, degrees");
+  graph_del->SetMinimum(0.);
+  graph_del->SetMaximum(60.);
+  graph_del->Draw("AC");
+
+  TGraph* graph_del_15 = new TGraph("del_delres_eres_15_anu.dat");
+  graph_del_15->SetLineColor(kRed);
+  graph_del_15->SetLineWidth(3);
+  graph_del_15->Draw("C SAME");
+
+  TGraph* graph_del_1 = new TGraph("del_delres_eres_1_anu.dat");
+  graph_del_1->SetLineColor(kGreen);
+  graph_del_1->SetLineWidth(3);
+  graph_del_1->Draw("C SAME");
+
+  TGraph* graph_del_0 = new TGraph("del_delres_eres_0_anu.dat");
+  graph_del_0->SetLineColor(kBlack);
+  graph_del_0->SetLineWidth(3);
+  graph_del_0->Draw("C SAME");
+
+}
