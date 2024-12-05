e349a8ecce39aa4f76fb9bb2efa805a05b990a01
kirsanov <Mikhail.Kirsanov@cern.ch>
2024-12-05 23:36:20 +0100

    Adjust 2 figures

--- papers/P2O-NOTE-24-01/del_delres_mdet.C
+++ papers/P2O-NOTE-24-01/del_delres_mdet.C
@@ -1,13 +1,21 @@
 {
+  gStyle->SetTextFont(1);
+  //gStyle->SetLineWidth(3);
+  gStyle->SetFrameLineWidth(2);
+
   auto c1 = new TCanvas("del_delres_mdet"," ",200,10,900,600);
   c1->SetGrid();
 
   TGraph* graph_del = new TGraph("del_delres_mdet1.dat");
-  graph_del->SetTitle("#delta CP resolution for energy resolutions 0.3 (nominal) and 0.05");
+  //graph_del->SetTitle("#delta CP resolution for energy resolutions 0.3 (nominal) and 0.05");
+  graph_del->SetTitle("");
   graph_del->SetLineColor(kBlue);
   graph_del->SetLineWidth(3);
-  graph_del->GetXaxis()->SetTitle("Detector mass, mt");
-  graph_del->GetYaxis()->SetTitle("#Delta#delta, degrees");
+  //graph_del->GetXaxis()->SetTitle("Detector mass, mt");
+  graph_del->GetXaxis()->SetTitle("M_{D} [Mt]");
+  graph_del->GetXaxis()->SetTitleSize(1.2*graph_del->GetXaxis()->GetTitleSize());
+  graph_del->GetYaxis()->SetTitle("#Delta#delta [degrees]");
+  graph_del->GetYaxis()->SetTitleSize(1.2*graph_del->GetYaxis()->GetTitleSize());
   graph_del->SetMinimum(0.);
   graph_del->Draw("AC");
 
