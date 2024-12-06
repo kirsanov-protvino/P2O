2f8f8df3996d0605d10748a078d5228911d06f58
kirsanov <Mikhail.Kirsanov@cern.ch>
2024-12-06 11:35:13 +0100

    Update 3 figures

--- papers/P2O-NOTE-24-01/del_delres_anufrac.C
+++ papers/P2O-NOTE-24-01/del_delres_anufrac.C
@@ -1,4 +1,8 @@
 {
+  gStyle->SetTextFont(1);
+  //gStyle->SetLineWidth(3);
+  gStyle->SetFrameLineWidth(2);
+
   auto c1 = new TCanvas("del_delres_anufrac"," ",200,10,900,600);
   c1->SetGrid();
 
@@ -6,8 +10,10 @@
   graph_del->SetTitle("");
   //graph_del->SetLineColor(kBlue);
   graph_del->SetLineWidth(3);
-  graph_del->GetXaxis()->SetTitle("Fraction of antineutrino in exposition");
-  graph_del->GetYaxis()->SetTitle("#Delta#delta, degrees");
+  graph_del->GetXaxis()->SetTitle("P(#bar #nu_{#mu})");
+  graph_del->GetXaxis()->SetTitleSize(1.15*graph_del->GetXaxis()->GetTitleSize());
+  graph_del->GetYaxis()->SetTitle("#Delta#delta [degrees]");
+  graph_del->GetYaxis()->SetTitleSize(1.2*graph_del->GetYaxis()->GetTitleSize());
   graph_del->SetMinimum(0.);
   graph_del->Draw("AC");
 }
--- papers/P2O-NOTE-24-01/del_delres_eres_anu.C
+++ papers/P2O-NOTE-24-01/del_delres_eres_anu.C
@@ -1,4 +1,6 @@
 {
+  gStyle->SetFrameLineWidth(2);
+
   auto c1 = new TCanvas("del_delres_eres_anu"," ",200,10,900,600);
   c1->SetGrid();
 
@@ -6,8 +8,10 @@
   graph_del->SetTitle("");
   graph_del->SetLineColor(kBlue);
   graph_del->SetLineWidth(3);
-  graph_del->GetXaxis()->SetTitle("Energy resolution #DeltaE/E");
-  graph_del->GetYaxis()->SetTitle("#Delta#delta, degrees");
+  graph_del->GetXaxis()->SetTitle("#DeltaE/E");
+  graph_del->GetXaxis()->SetTitleSize(1.2*graph_del->GetXaxis()->GetTitleSize());
+  graph_del->GetYaxis()->SetTitle("#Delta#delta [degrees]");
+  graph_del->GetYaxis()->SetTitleSize(1.2*graph_del->GetYaxis()->GetTitleSize());  
   graph_del->SetMinimum(0.);
   graph_del->SetMaximum(60.);
   graph_del->Draw("AC");
